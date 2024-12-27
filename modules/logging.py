1b9b1b21faed935eff05b0cc49949209197416c2
Kevin Kamto <kamtokevin@gmail.com>
2024-12-27 10:19:34 -0500

    chore: remove unnecessary modules

--- modules/logging.py
+++
@@ -1,3 +0,0 @@
-# modules/logging.py
-def log_data(data):
-    print(f"Logged Data: {data}")
--- modules/processing.py
+++
@@ -1,3 +0,0 @@
-# modules/processing.py
-def process_data(raw_data):
-    return {k: round(v, 2) for k, v in raw_data.items()}
